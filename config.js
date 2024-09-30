0c37d59ab2a434696f693eab65fc8c78e55be084
yangyi <107008983+yygzs2yiyang@users.noreply.github.com>
2024-09-30 14:23:54 +0800

    Init

---
+++ config.js
@@ -0,0 +1,34 @@
+﻿window.Config = {
+
+  // 显示标题
+  SiteName: 'Your Title',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'APIKEY'],
+
+  // 日志天数
+  CountDays: 30,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '站点1',
+      url: 'https://url.com'
+    },
+    
+    {
+      text: '站点2',
+      url: 'https://baidu.com'
+    },
+,
+    
+    {
+      text: 'GitPage',
+      url: 'http://sss.sss'
+    }  ],
+};
